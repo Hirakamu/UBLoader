b5cf0ffd539725b7608a3baac30ee6235fbe2cdb
Hirakamu <achmadirsyadhamizan@gmail.com>
2025-09-27 14:20:33 +0700

    Localized github links

--- index/Key.lua
+++ index/Key.lua
@@ -1,9 +1,9 @@
 getgenv().keyEntered = true
-local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/UBLoader/refs/heads/main/index/Games.lua"))()
+local Games = loadstring(game:HttpGet("https://raw.githubusercontent.com/Hirakamu/UBLoader/refs/heads/main/index/Games.lua"))()
 local gameScript = Games[game.PlaceId] 
 if gameScript then
     loadstring(game:HttpGet(gameScript))()
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/TeamUBHub/Script-Roblox/refs/heads/main/UniversalScript.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Hirakamu/Script-Roblox/refs/heads/main/UniversalScript.lua"))()
 end
-loadstring(game:HttpGet("https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/JoinUI.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/rhuda21/Main/refs/heads/main/Library/JoinUI.lua"))()
